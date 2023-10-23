6e0caf587376dcb60b4f5c6dea8b35450ec36ed9
Joshua Oladele <jurshsmith@gmail.com>
2023-10-23 20:09:30 +0100

    Index created games

--- coinflip-web/src/app_workers/index_evm_states.rs
+++ coinflip-web/src/app_workers/index_evm_states.rs
@@ -13,28 +13,19 @@ pub struct IndexEvmStates;
 impl IndexEvmStates {
     pub fn start() {
         task::spawn(async {
-            let bayc_contract =  Contract::new("BoredApeYachtClub")
-            .add_event("event Transfer(address indexed from, address indexed to, uint256 indexed tokenId)", TransferEventHandler)
-            .add_state_migrations(NftStateMigrations)
-            .add_address(
-                "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D",
-                &Chain::Mainnet,
-                17773490,
-            );
-            let doodles_contract =  Contract::new("Doodles")
-            .add_event("event Transfer(address indexed from, address indexed to, uint256 indexed tokenId)", TransferEventHandler)
-            .add_address(
-                "0x8a90CAb2b38dba80c64b7734e58Ee1dB38B8992e",
-                &Chain::Mainnet,
-                17769635,
-            );
+            let coinflip_contract = Contract::new("Coinflip")
+                .add_event(
+                    "event GameCreated(uint256 gameID, uint16 maxPlayCount, uint256 expiryTimestamp)",
+                    GameCreatedEventHandler,
+                )
+                .add_state_migrations(NftStateMigrations)
+                .add_address("0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0", &Chain::Dev, 0);
 
             let config = chaindexing::Config::new(
                 chaindexing::PostgresRepo::new(&DB::url()),
                 Self::chains(),
             )
-            .add_contract(bayc_contract)
-            .add_contract(doodles_contract);
+            .add_contract(coinflip_contract);
 
             Chaindexing::index_states(&config).await.unwrap();
         });
@@ -44,22 +35,22 @@ impl IndexEvmStates {
         dotenvy::dotenv().ok();
 
         HashMap::from([(
-            Chain::Mainnet,
-            std::env::var("MAINNET_JSON_RPC_URL").expect("MAINNET_JSON_RPC_URL must be set"),
+            Chain::Dev,
+            std::env::var("LOCAL_JSON_RPC_URL").expect("LOCAL_JSON_RPC_URL must be set"),
         )])
     }
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
-struct NftState {
-    token_id: i32,
-    contract_address: String,
-    owner_address: String,
+struct CoinflipGame {
+    id: u64,
+    max_play_count: u32,
+    expiry_timestamp: u64,
 }
 
-impl ContractState for NftState {
+impl ContractState for CoinflipGame {
     fn table_name() -> &'static str {
-        "nft_states"
+        "coinflip_games"
     }
 }
 
@@ -68,48 +59,40 @@ struct NftStateMigrations;
 impl ContractStateMigrations for NftStateMigrations {
     fn migrations(&self) -> Vec<&'static str> {
         vec![
-            "CREATE TABLE IF NOT EXISTS nft_states (
-                token_id INTEGER NOT NULL,
-                contract_address TEXT NOT NULL,
-                owner_address TEXT NOT NULL
+            "CREATE TABLE IF NOT EXISTS coinflip_games (
+                id BIGINT NOT NULL,
+                max_play_count INTEGER NOT NULL,
+                expiry_timestamp BIGINT NOT NULL
             )",
         ]
     }
 }
 
-struct TransferEventHandler;
+struct GameCreatedEventHandler;
 
 #[async_trait::async_trait]
-impl EventHandler for TransferEventHandler {
+impl EventHandler for GameCreatedEventHandler {
     async fn handle_event<'a>(&self, event_context: EventContext<'a>) {
         let event = &event_context.event;
         let event_params = event.get_params();
 
-        let from = event_params.get("from").unwrap().clone().into_address().unwrap();
-        let to = event_params.get("to").unwrap().clone().into_address().unwrap();
-        let token_id = event_params.get("tokenId").unwrap().clone().into_uint().unwrap();
+        let id = event_params.get("gameID").unwrap().clone().into_uint().unwrap().as_u64();
+        let max_play_count =
+            event_params.get("maxPlayCount").unwrap().clone().into_uint().unwrap().as_u32();
+        let expiry_timestamp = event_params
+            .get("expiryTimestamp")
+            .unwrap()
+            .clone()
+            .into_uint()
+            .unwrap()
+            .as_u64();
 
-        if let Some(nft_state) = NftState::read_one(
-            [
-                ("token_id".to_owned(), token_id.to_string()),
-                ("owner_address".to_owned(), from.to_string()),
-            ]
-            .into(),
-            &event_context,
-        )
-        .await
-        {
-            let updates = [("owner_address".to_string(), to.to_string())];
-
-            nft_state.update(updates.into(), &event_context).await;
-        } else {
-            NftState {
-                token_id: token_id.as_u32() as i32,
-                contract_address: event.contract_address.clone(),
-                owner_address: to.to_string(),
-            }
-            .create(&event_context)
-            .await;
+        CoinflipGame {
+            id,
+            max_play_count,
+            expiry_timestamp,
         }
+        .create(&event_context)
+        .await;
     }
 }

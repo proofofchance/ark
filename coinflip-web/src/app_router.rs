b770698fdcf5b0bcc9aed0d8299ec4dcd1f1cbd7
Joshua Oladele <jurshsmith@gmail.com>
2023-10-25 14:32:33 +0100

    Relax CORS for frontend

--- coinflip-web/src/app_router.rs
+++ coinflip-web/src/app_router.rs
@@ -1,5 +1,10 @@
 use ark_web_app::AppState;
 use axum::{routing::get, Router};
+use http::{
+    header::{ACCEPT, ACCESS_CONTROL_ALLOW_HEADERS, AUTHORIZATION, CONTENT_TYPE},
+    HeaderValue,
+};
+use tower_http::cors::{Any, CorsLayer};
 
 use crate::handlers::game_handler;
 
@@ -10,7 +15,7 @@ pub struct AppRouter {
 impl AppRouter {
     pub fn new() -> Self {
         Self {
-            routes: Router::new().merge(Self::game_routes()),
+            routes: Router::new().merge(Self::game_routes()).layer(Self::cors_layer()),
         }
     }
 
@@ -19,5 +24,22 @@ impl AppRouter {
             "/games",
             Router::new().route("/", get(game_handler::get_games)),
         )
+    }
+
+    fn cors_layer() -> CorsLayer {
+        dotenvy::dotenv().ok();
+
+        let frontend_origin =
+            std::env::var("FRONTEND_ORIGIN").expect("FRONTEND_ORIGIN must be set");
+
+        CorsLayer::new()
+            .allow_origin(frontend_origin.parse::<HeaderValue>().unwrap())
+            .allow_methods(Any)
+            .allow_headers([
+                ACCEPT,
+                ACCESS_CONTROL_ALLOW_HEADERS,
+                AUTHORIZATION,
+                CONTENT_TYPE,
+            ])
     }
 }

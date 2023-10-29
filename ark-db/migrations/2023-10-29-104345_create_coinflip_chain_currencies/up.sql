20607ceb6234ff8423cfec41b3f35b6898bddab3
Joshua Oladele <jurshsmith@gmail.com>
2023-10-29 23:44:02 +0100

    Fix typo

--- ark-db/migrations/2023-10-29-104345_create_coinflip_chain_currencies/up.sql
+++ ark-db/migrations/2023-10-29-104345_create_coinflip_chain_currencies/up.sql
@@ -3,5 +3,5 @@ CREATE TABLE coinflip_chain_currencies (
                 id SERIAL PRIMARY KEY,
                 chain_id INTEGER NOT NULL,
                 currency_symbol VARCHAR NOT NULL,
-                unit_value_in_usd VARCHAR NOT NULL
+                unit_price_in_usd VARCHAR NOT NULL
             )   

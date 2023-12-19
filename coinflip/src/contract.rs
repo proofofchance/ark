37032db8762dd66ae40139154b2e473ecd199ee9
Joshua Oladele <jurshsmith@gmail.com>
2023-12-19 22:45:03 +0100

    Ignore proofCreated event for now
    This event should eventually become proofsCreated (plural) since
    the server uploads the proofs at once

--- coinflip/src/contract.rs
+++ coinflip/src/contract.rs
@@ -11,11 +11,8 @@ use states::{GameActivityMigrations, GameMigrations, GamePlayMigrations};
 
 use dotenvy::dotenv;
 
-pub struct CoinflipContract;
-
-impl CoinflipContract {
-    pub fn get() -> Contract {
-        Contract::new("Coinflip")
+pub fn get() -> Contract {
+    Contract::new("Coinflip")
         .add_event(
             "event GameCreated(uint256 gameID, uint16 maxPlayCount, uint256 expiryTimestamp, address creator, uint256 wager)",
             GameCreatedEventHandler,
@@ -24,19 +21,18 @@ impl CoinflipContract {
             "event GamePlayCreated(uint16 gamePlayID, uint256 gameID, uint8 coinSide, address player, bytes32 playHash)",
             GamePlayCreatedEventHandler,
         )
-        .add_event(
-            "event GamePlayProofCreated(uint16 gamePlayID, uint256 gameID, address player, string playProof)",
-            GamePlayProofCreatedEventHandler,
-        )
+        // .add_event(
+        //     "event GamePlayProofCreated(uint16 gamePlayID, uint256 gameID, address player, string playProof)",
+        //     GamePlayProofCreatedEventHandler,
+        // )
         .add_state_migrations(GameMigrations)
         .add_state_migrations(GamePlayMigrations)
         .add_state_migrations(GameActivityMigrations)
-        .add_address(&Self::address(), &Chain::Dev, 0)
-    }
+        .add_address(&address(), &Chain::Dev, 0)
+}
 
-    fn address() -> String {
-        dotenv().ok();
+fn address() -> String {
+    dotenv().ok();
 
-        std::env::var("COINFLIP_ADDRESS").expect("COINFLIP_ADDRESS must be set")
-    }
+    std::env::var("COINFLIP_ADDRESS").expect("COINFLIP_ADDRESS must be set")
 }
--- coinflip/src/lib.rs
+++ coinflip/src/lib.rs
@@ -1,9 +1,8 @@
 pub mod chains;
 mod coin;
-mod contract;
+pub mod contract;
 mod games;
 
 pub use chains::Chain;
 pub use coin::*;
-pub use contract::CoinflipContract;
 pub use games::*;

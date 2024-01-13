1b0e935a1ea95fcee3bad5bbea495fb74ab8888c
Joshua Oladele <jurshsmith@gmail.com>
2024-01-13 09:01:46 +0100

    Update to chaindexing v0.1.25

--- coinflip-web/src/app_workers/index_evm_states.rs
+++ coinflip-web/src/app_workers/index_evm_states.rs
@@ -7,12 +7,10 @@ use chaindexing::{Chain, Chaindexing, Repo};
 
 pub fn start(db_pool: Arc<DBPool>) {
     tokio::spawn(async {
-        let config = chaindexing::Config::new(
-            chaindexing::PostgresRepo::new(&ark_db::url()),
-            Some(db_pool),
-        )
-        .add_chain(Chain::Dev, &get_local_json_rpc_url())
-        .add_contract(coinflip_contracts::contract::get());
+        let config = chaindexing::Config::new(chaindexing::PostgresRepo::new(&ark_db::url()))
+            .with_initial_state(db_pool)
+            .add_chain(Chain::Dev, &get_local_json_rpc_url())
+            .add_contract(coinflip_contracts::contract::get());
 
         Chaindexing::index_states(&config).await.unwrap();
     });

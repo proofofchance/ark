d432dbd9c64cc94917833a2afc9d3952ebe878e5
Joshua Oladele <jurshsmith@gmail.com>
2024-01-28 14:43:17 +0100

    Remove cors layer from coinflip-web

--- coinflip-web/src/app_router.rs
+++ coinflip-web/src/app_router.rs
@@ -3,11 +3,6 @@ use axum::{
     routing::{get, put},
     Router,
 };
-use http::{
-    header::{ACCEPT, ACCESS_CONTROL_ALLOW_HEADERS, AUTHORIZATION, CONTENT_TYPE},
-    HeaderValue,
-};
-use tower_http::cors::{Any, CorsLayer};
 
 use crate::handlers::{game_activity_handler, game_handler, game_play_handler};
 
@@ -21,8 +16,7 @@ impl AppRouter {
             routes: Router::new()
                 .merge(Self::game_routes())
                 .merge(Self::game_play_routes())
-                .merge(Self::game_activty_routes())
-                .layer(Self::cors_layer()),
+                .merge(Self::game_activty_routes()),
         }
     }
 
@@ -54,22 +48,5 @@ impl AppRouter {
                 get(game_activity_handler::get_ongoing_game_activities),
             ),
         )
-    }
-
-    fn cors_layer() -> CorsLayer {
-        dotenvy::dotenv().ok();
-
-        let frontend_origin =
-            std::env::var("FRONTEND_ORIGIN").expect("FRONTEND_ORIGIN must be set");
-
-        CorsLayer::new()
-            .allow_origin(frontend_origin.parse::<HeaderValue>().unwrap())
-            .allow_methods(Any)
-            .allow_headers([
-                ACCEPT,
-                ACCESS_CONTROL_ALLOW_HEADERS,
-                AUTHORIZATION,
-                CONTENT_TYPE,
-            ])
     }
 }

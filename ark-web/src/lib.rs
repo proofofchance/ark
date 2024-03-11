5a4bc3fc6466dc40110793e162abd72746a53a6a
Joshua Oladele <jurshsmith@gmail.com>
2024-03-11 21:55:38 +0100

    Remove unused server_responses module

--- ark-web/src/lib.rs
+++ ark-web/src/lib.rs
@@ -2,8 +2,6 @@ mod app_router;
 mod app_server_config;
 pub mod app_workers;
 mod handlers;
-mod server_responses;
 
 pub use app_router::AppRouter;
 pub use app_server_config::AppServerConfig;
-pub use server_responses::ServerDataResponse;
